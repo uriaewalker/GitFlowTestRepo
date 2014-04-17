26da628a9511f71cc0792d7f38bb352370ab2b56
uriaewalker <uriae.walker@developmentnow.com>
2014-04-17 09:54:02 -0700

    added superman feature

--- myFakeprogram.c
+++ myFakeprogram.c
@@ -5,6 +5,7 @@ main()
 	printf("Hello, world!\n");
 	printf("changes made in develop branch!"\n);
 	printf("new code I added while in my feature branch!\n");
+	printf("new code added in feature/superman branch!\n");
 
 return 0;
 }
